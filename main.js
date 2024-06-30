541bd78f2a1ae9a402235cbf1c924f1b98b37580
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:39:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// if else statement
+var admissioncriteria = 70;
+if (admissioncriteria >= 80) {
+    console.log("You are eligible");
+}
+else {
+    console.log("You are not eligible");
+}
+// if else statements (fb insta logine method)
+var enterEmail = false;
+var enterNumber = false;
+if (enterEmail || enterNumber) {
+    console.log("Continue");
+}
+else {
+    console.log("invalid email");
+}
+// if else and else if statements
+/*let per=120
+if (per>=90 && per<=100){
+console.log("your grade is A+");
+}
+else if(per>=80 && per<90){
+console.log("your grade is A");
+}
+else if(per>=70 && per<80){
+console.log("your grade is B+");
+}
+else if(per>=60 && per<70){
+console.log("your grade is B");
+}
+else if(per>=50 && per<60){
+console.log("your grade is C");
+}
+else if (per>=1 && per<=49){
+console.log("you are fail");
+}
+else {
+    console.log("you have entered an invalid percentage")
+}*/
+// if and else if statements
+/*let weather="sunny"
+if(weather=="cloudy"){
+console.log("wear a light jacket");
+}
+else if(weather=="rainy"){
+console.log("wear a rain coat or take an umbrella")
+}
+else if(weather=="windy"){
+console.log("wear a cover")
+}
+else{
+    console.log("wear sunglesses")
+}*/
+//Ternary operators
+var hungry = true;
+var snack = hungry ? "pizza" : "water";
+console.log('you should have some ${snack}');
